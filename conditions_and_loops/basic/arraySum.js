13770b2956ad4a5a5810fd023f8b98d93b1a5f28
Zitan Salehin <zitan-74@dipti.com.bd>
2024-05-04 21:08:53 +0600

    array sum if not negative

---
+++ conditions_and_loops/basic/arraySum.js
@@ -0,0 +1,14 @@
+function sumOfPositives(numbers) {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+            sum += numbers[i];
+        }
+    }
+
+    return sum;
+}
+
+// Example usage:
+console.log(sumOfPositives([1, -4, 7, 12]));
